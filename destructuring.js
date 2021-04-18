5fdcd9d1d73868ffbe5a05d2299f17f2baee5cd6
Arntje <a.braakenburg@chello.nl>
2021-04-18 11:32:58 +0200

    klaar

--- destructuring.js
+++ destructuring.js
@@ -6,6 +6,9 @@ const product = {
   screenType: 'LED',
 }
 
+const {price, refreshRate} = product;
+console.log("Price: " + price + "\n" + "Refresh rate: " + refreshRate);
+
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -17,6 +20,9 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+const {type, brand} = productInformation.general;
+console.log("Type: " + type);
+console.log("Brand: " + brand);
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -37,16 +43,21 @@ const tvOptions = {
     }
   },
 };
+const {wifi, bluetooth} = tvOptions.options.connectivity.popular;
+console.log("wifi: " + wifi);
+console.log("bluetooth: " + bluetooth);
 
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
-
+const [first, second] = connectivity;
+console.log(first, second);
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
-
+const [first1, second1, third, fourth, fifth] = tvSizes;
+console.log(first1, third);
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
 
@@ -56,9 +67,15 @@ function getCompanyDetails() {
     adress: 'Zonnebaan 9, Utrecht',
   }
 }
+const {name: companyName, adress: companyAdress} = getCompanyDetails();
+console.log(companyName);
+console.log(companyAdress);
+
 
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+const [name, age] = getDetails("Arnt", 50);
+console.log(name, age);
